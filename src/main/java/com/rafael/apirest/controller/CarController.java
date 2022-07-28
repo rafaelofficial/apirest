c1a80e83514cc942c9a6bc3177bf82f67809ef44
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2022-07-28 18:30:30 -0300

    operation of instantiation from databases

--- src/main/java/com/rafael/apirest/controller/CarController.java
+++ src/main/java/com/rafael/apirest/controller/CarController.java
@@ -5,22 +5,22 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rafael.apirest.model.Car;
 import com.rafael.apirest.services.CarService;
 
 @RestController
+@RequestMapping(value="api/cars")
 public class CarController {
 	
 	@Autowired
 	private CarService service;
 
-	@GetMapping("/cars")
+	@GetMapping
 	public ResponseEntity<List<Car>> findAll() {		
 		List<Car> list = service.findAll();
 		return ResponseEntity.ok(list);
-	}
-	
-	
+	}	
 }
