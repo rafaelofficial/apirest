0da88163c392632d009f5cb73246aa3ff59620a2
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2022-08-01 12:34:13 -0300

    implemetation method insert with POST

--- src/main/java/com/rafael/apirest/controller/CarController.java
+++ src/main/java/com/rafael/apirest/controller/CarController.java
@@ -1,13 +1,17 @@
 package com.rafael.apirest.controller;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rafael.apirest.dto.CarDTO;
 import com.rafael.apirest.model.Car;
@@ -26,5 +30,13 @@ public class CarController {
 		
 		List<CarDTO> listDto = list.stream().map(x -> new CarDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@PostMapping("/createCar")
+	public ResponseEntity<Void> insert(@RequestBody CarDTO objDto) {
+		Car obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/rafael/apirest/services/CarService.java
+++ src/main/java/com/rafael/apirest/services/CarService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.rafael.apirest.dto.CarDTO;
 import com.rafael.apirest.model.Car;
 import com.rafael.apirest.repository.CarRepository;
 
@@ -16,5 +17,14 @@ public class CarService {
 
 	public List<Car> findAll() {
 		return repository.findAll();
+	}
+	
+	public Car insert(Car obj) {
+		return repository.insert(obj);
+	}
+	
+	// get from CarDTO and instance in Car
+	public Car fromDTO(CarDTO objDto) {
+		return new Car(objDto.getId(), objDto.getTitle(), objDto.getBrand(), objDto.getPrice(), objDto.getAge());
 	}
 }
